044074fe10bc561f0eaa602e791adf3aba5d89cc
Diego-Batista <diegobtistadev@gmail.com>
2025-05-18 12:59:55 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+
+const server = express();
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static("public"));
+
+server.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+server.listen(4444, () => {
+  console.log("Server is running on http://localhost:3000");
+});
